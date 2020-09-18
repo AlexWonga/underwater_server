3786e69b478a72774511f7bba1e2857c22efdb62
Alex_Wonga <494217470@qq.com>
2020-09-18 15:31:02 +0800

    上传二维数据参数是toString，使用is_number判断参数

--- src/router/TwoDimensionalDataMiddleware.ts
+++ src/router/TwoDimensionalDataMiddleware.ts
@@ -4,16 +4,18 @@ import {invalidParameter} from "./invalidParameter";
 import {deleteTwoDimensionalData, uploadTwoDimensionalData} from "../server/TwoDimensionalServer";
 import {IContext, ISession, IState} from "../interface/session";
 import {checkDvSupSession} from "./checkPermissionMiddleware";
+import is_number from "is-number";
 // import send from "koa-send";
 // import {rootDirPath} from "../config/filePaths";
 // import path from "path";
 
 module.exports = (router: Router<IContext, IState>) => {
     router.post('/api/uploadTwoDimensionalData', checkDvSupSession, async (ctx) => {
-        if (typeof ctx.request.body.deviceID !== 'number' || typeof ctx.request.body.categoryID !== 'number'||!ctx.request.files) {
+        if (!is_number(ctx.request.body.deviceID) || !is_number(ctx.request.body.categoryID)||!ctx.request.files) {
             ctx.body = invalidParameter();
         } else {
-            const {deviceID,categoryID} = ctx.request.body;
+            let {deviceID,categoryID} = ctx.request.body;
+            deviceID = Number(deviceID);categoryID = Number(categoryID);
             const {xlsxFile} = ctx.request.files;
             const {userID} = ctx.session.data as ISession;
             const response = await uploadTwoDimensionalData(xlsxFile,deviceID,categoryID,userID);
