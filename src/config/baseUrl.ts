582d7e3bcdc54e4e4f9afdad14103ff0538bc64b
Alex_Wonga <494217470@qq.com>
2020-09-29 18:22:06 +0800

    http to https

--- src/config/baseUrl.ts
+++ src/config/baseUrl.ts
@@ -6,4 +6,5 @@
 // export const baseURL = "//127.0.0.1:8000";
 
 // export const baseURL = "http://119.3.212.206";
-export const baseURL = "http://172.6.2.180";
+// export const baseURL = "http://172.6.2.180";
+export const baseURL = "https://172.6.2.180";
