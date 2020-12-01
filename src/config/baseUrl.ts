cb82dd9c8a98c23a2945764a45dfcaf5e9a521ba
Alex_Wonga <494217470@qq.com>
2020-12-01 23:31:06 +0800

    更改baseURL

--- src/config/baseUrl.ts
+++ src/config/baseUrl.ts
@@ -1,11 +1,11 @@
 /**
- * @description 绝对地址转换为网络可访问地址的前段基本地址 ip+端口号
+ * @description 绝对地址转换为网络可访问地址的前段基本地址
  */
 
 
 // export const baseURL = "//127.0.0.1:8000";
 
-export const baseURL = "http://119.3.212.206";
+// export const baseURL = "http://119.3.212.206";
 // export const baseURL = "http://172.6.2.180";
 // export const baseURL = "https://172.6.2.180";
-// export const baseURL = "https://www.foril.fun";
+export const baseURL = "https://www.foril.fun";
