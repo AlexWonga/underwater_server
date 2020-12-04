fa808edafe1a37c8fe5555ba7eedf2cb1cab7769
Alex_Wonga <494217470@qq.com>
2020-12-04 15:06:35 +0800

    修改密码错误验证码不刷新的bug

--- src/router/UserSupervisorMiddleware.ts
+++ src/router/UserSupervisorMiddleware.ts
@@ -40,13 +40,19 @@ module.exports = (router: Router<IState, IContext>) => {
             if (text === null || text.toLowerCase() !== code.toString().toLowerCase()) {
                 ctx.body = new ResponseBody<void>(false, 'wrongVerificationCode');
             } else {
-                //console.log(username, password);
                 const response = await supervisorLogin(username, password);//服务层返回的response
                 if (response.session) {//如果session不为空 就设置session
                     ctx.session.data = response.session;
                 }
                 const {isSuccessful, message} = response.body;
-                ctx.body = new ResponseBody<void>(isSuccessful, message);
+                if (!isSuccessful && message === "wrongPassword") {
+                    let captcha = svgCaptcha.create();
+                    let {text, data} = captcha;
+                    ctx.session.text = text;
+                    ctx.body = {boolean: isSuccessful, string: message, svgData: data};
+                } else {
+                    ctx.body = new ResponseBody<void>(isSuccessful, message);
+                }
             }
         }
         // await next();
